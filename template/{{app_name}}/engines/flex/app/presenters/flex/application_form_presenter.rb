b27fbd31595ec4f926733ebb77d4dcde2f2b06a4
s1rius710 <sirius.710@outlook.com>
2025-05-06 17:42:45 -0700

    Revert "Create Presenter base class"
    This reverts commit a91c3b309d2bf24fa30cf0ad77c3631a0fc724d3.

--- template/{{app_name}}/engines/flex/app/presenters/flex/application_form_presenter.rb
+++ template/{{app_name}}/engines/flex/app/presenters/flex/application_form_presenter.rb
@@ -1,29 +1,28 @@
 module Flex
-  class ApplicationFormPresenter < Flex::Presenter
-    attr_reader :application_form
-
+  class ApplicationFormPresenter
     def initialize(view_context, application_form)
-      super(view_context)
+      @view_context = view_context
       @application_form = application_form
+      @i18n_path = view_context.controller_path.gsub("/", ".")
     end
 
     def index
       {
         created_at: created_at,
-        path: view_context.polymorphic_path(application_form),
+        path: @view_context.polymorphic_path(@application_form),
         status: status
       }
     end
 
     def show
       {
-        title: t("show.title"),
-        back_link_text:  t("show.back"),
-        index_path: view_context.polymorphic_path(application_form.class),
+        title: I18n.t("#{@i18n_path}.show.title"),
+        back_link_text:  I18n.t("#{@i18n_path}.show.back"),
+        index_path: @view_context.polymorphic_path(@application_form.class),
         created_at:  created_at,
-        current_status: application_form.status,
-        next_step: t("show.next_step.status.#{application_form.status}"),
-        submitted_on_text: t("show.submitted_on"),
+        current_status: @application_form.status,
+        next_step: I18n.t("#{@i18n_path}.show.next_step.status.#{@application_form.status}"),
+        submitted_on_text: I18n.t("#{@i18n_path}.show.submitted_on"),
         status: status
       }
     end
@@ -31,11 +30,11 @@ module Flex
     private
 
     def created_at
-      application_form.created_at.strftime("%B %d, %Y at %I:%M %p")
+      @application_form.created_at.strftime("%B %d, %Y at %I:%M %p")
     end
 
     def status
-      I18n.t("flex.application_forms.status.#{application_form.status}")
+      I18n.t("flex.application_forms.status.#{@application_form.status}")
     end
   end
 end
--- template/{{app_name}}/engines/flex/app/presenters/flex/application_forms_presenter.rb
+++ template/{{app_name}}/engines/flex/app/presenters/flex/application_forms_presenter.rb
@@ -1,19 +1,20 @@
 module Flex
-  class ApplicationFormsPresenter < Flex::Presenter
+  class ApplicationFormsPresenter
     attr_reader :application_forms
 
     def initialize(view_context, application_forms)
-      super(view_context)
+      @view_context = view_context
       @application_forms = application_forms
+      @i18n_path = view_context.controller_path.gsub("/", ".")
     end
 
     def index
       {
-        title: t("index.title"),
-        intro: t("index.intro"),
-        new_button_text: t("index.new_button"),
-        in_progress_applications_heading: t("index.in_progress_applications.heading"),
-        application_forms: application_forms.map { |application_form| Flex::ApplicationFormPresenter.new(view_context, application_form).index }
+        title: I18n.t("#{@i18n_path}.index.title"),
+        intro: I18n.t("#{@i18n_path}.index.intro"),
+        new_button_text: I18n.t("#{@i18n_path}.index.new_button"),
+        in_progress_applications_heading: I18n.t("#{@i18n_path}.index.in_progress_applications.heading"),
+        application_forms: application_forms.map { |application_form| Flex::ApplicationFormPresenter.new(@view_context, application_form).index }
       }
     end
   end
