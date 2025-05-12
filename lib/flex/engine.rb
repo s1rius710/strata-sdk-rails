19de3443619b6c67acbc761bc81f085bb08d5eff
s1rius710 <sirius.710@outlook.com>
2025-05-12 14:25:42 -0700

    Add Flex previews to host application's lookbook

--- lib/flex/engine.rb
+++ lib/flex/engine.rb
@@ -7,5 +7,9 @@ module Flex
         helper Flex::ApplicationHelper
       end
     end
+
+    initializer "flex.previews" do |app|
+      config.lookbook.preview_paths << Flex::Engine.root.join("app", "previews") if config.respond_to?(:lookbook)
+    end
   end
 end
