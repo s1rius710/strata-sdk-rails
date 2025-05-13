3de17be17a26068fbd42eebf14f65507b590d4ad
s1rius710 <sirius.710@outlook.com>
2025-05-13 11:03:13 -0700

    Move application form previews to flex folder (#64)
    - Move the application form and step indicator previews to the flex
    engine so that the host application can also inherit / benefit from
    these lookbook previews
    - Rename previews from FlexSomethingPreview to SomethingPreview but put
    it in the module Flex namespace
    
    ## Context
    
    These previews were previously in spec/dummy/app/previews which means
    the host application can't see them but I think they are pretty useful
    for host application developers to see since these are for reusable
    views
    
    I already added [this
    commit](https://github.com/navapbc/flex-sdk/commit/90c50993153a9bd9da03615030b320b0fe3af313)
    which adds the engine's previews to the host application's lookbook (if
    they have lookbook)

---
+++ app/previews/flex/application_forms_index_preview.rb
@@ -0,0 +1,34 @@
+module Flex
+  class ApplicationFormsIndexPreview < Lookbook::Preview
+    def empty
+      render template: "flex/application_forms/index", locals: {
+        title: "My Applications",
+        intro: "Start a new application or continue an existing one.",
+        new_button_text: "Start New Application",
+        in_progress_applications_heading: "Your Applications",
+        application_forms: []
+      }
+    end
+
+    def with_applications
+      render template: "flex/application_forms/index", locals: {
+        title: "My Applications",
+        intro: "Start a new application or continue an existing one.",
+        new_button_text: "Start New Application",
+        in_progress_applications_heading: "Your Applications",
+        application_forms: [
+          {
+            created_at: "2024-01-15",
+            path: "/applications/1",
+            status: :in_progress
+          },
+          {
+            created_at: "2024-01-10",
+            path: "/applications/2",
+            status: :submitted
+          }
+        ]
+      }
+    end
+  end
+end
---
+++ app/previews/flex/step_indicator_preview.rb
@@ -0,0 +1,30 @@
+module Flex
+  class StepIndicatorPreview < Lookbook::Preview
+    layout "component_preview"
+
+    # @!group Statuses
+
+    def in_progress
+      render template: "flex/shared/_step_indicator", locals: {
+        steps: [ :in_progress, :submitted, :decision_made ],
+        current_step: :in_progress
+      }
+    end
+
+    def submitted
+      render template: "flex/shared/_step_indicator", locals: {
+        steps: [ :in_progress, :submitted, :decision_made ],
+        current_step: :submitted
+      }
+    end
+
+    def decision_made
+      render template: "flex/shared/_step_indicator", locals: {
+        steps: [ :in_progress, :submitted, :decision_made ],
+        current_step: :decision_made
+      }
+    end
+
+    # @!endgroup
+  end
+end
