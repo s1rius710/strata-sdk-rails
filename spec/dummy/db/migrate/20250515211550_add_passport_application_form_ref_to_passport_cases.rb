69a4be876e17530f6ba2aa569f0074da0528bae6
s1rius710 <sirius.710@outlook.com>
2025-05-16 16:04:28 -0700

    Remove extra line from migration

--- spec/dummy/db/migrate/20250515211550_add_passport_application_form_ref_to_passport_cases.rb
+++ spec/dummy/db/migrate/20250515211550_add_passport_application_form_ref_to_passport_cases.rb
@@ -1,6 +1,5 @@
 class AddPassportApplicationFormRefToPassportCases < ActiveRecord::Migration[8.0]
   def change
-    execute "DELETE FROM passport_cases"
     add_reference :passport_cases, :application_form, type: :string
   end
 end
